c6c4c04cecf85aa6740925a383409dea91c71a84
tsteward0@gmail.com <tsteward0@gmail.com>
2019-11-02 16:06:22 -0500

    check user name correctly

--- ui/src/app/auth/auth.service.ts
+++ ui/src/app/auth/auth.service.ts
@@ -1,24 +1,54 @@
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
-import {HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
-  private authenticated = false;
+  private authenticated = null;
 
   constructor(private http: HttpClient) { }
 
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json'
+    })
+  };
+
   public isAuthenticated(): boolean {
+    return this.authenticated != null;
+  }
+
+  public authenticate(username: string, password: string): Promise<boolean> {
+    const body: { username: string; password: string; } = {username, password};
+
+    return new Promise<boolean>((resolve, reject) => {
+      this.http.post(`http://${environment.server}/login`, JSON.stringify(body), this.httpOptions).subscribe((resp) => {
+        if ((resp as Response).username) {
+          this.authenticated = (resp as Response).username;
+          console.log('false');
+          resolve(true);
+        } else {
+          console.log('false');
+          resolve(false);
+        }
+      },
+      (err) => {
+        resolve(false);
+      });
+    });
+  }
+
+  public getAuthenticatedUser(): string {
     return this.authenticated;
   }
 
-  public authenticate(username: string, password: string) {
-    this.authenticated = true;
-  }
-
   public createUser(username: string, password: string) {
-    
+
   }
+}
+
+class Response {
+  username: string;
 }
