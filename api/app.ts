91d4193d5f282ba1112f067d47a36b237ee46a8b
Joseph Naberhaus <naberhausj@gmail.com>
2019-11-02 10:18:59 -0500

    Complete change to firebase

--- api/app.ts
+++ api/app.ts
@@ -1,5 +1,4 @@
 import express = require('express');
-import { MongoClient } from "mongodb";
 const { Firestore } = require("@google-cloud/firestore");
 
 const cors = require("cors");
@@ -9,43 +8,6 @@ const firestore = new Firestore({projectId: "foodbankexchange", keyFilename: 'gc
 
 // Create a new express application instance
 const app: express.Application = express();
-
-// Database initialization routine
-const initializeDatabase = (callback: (err: any) => void) => {
-    console.group('Initializing application database');
-    console.log(`Connecting to ${MONGO_SERVER}...`);
-    MongoClient.connect(MONGO_SERVER, { useUnifiedTopology: true }, function (err, client) {
-        if (err) {
-            console.error(`Failed to connect to server ${MONGO_SERVER}.`, err);
-            console.groupEnd();
-            callback(true);
-            return;
-        }
-
-        console.log(`Connected to ${MONGO_SERVER}.`);
-        console.log(`Selecting database ${MONGO_DATABASE}.`);
-        db = client.db(MONGO_DATABASE);
-
-        const collectionsToCheck = [ 'foodbanks' ];
-        console.group(`Checking for collections: ` + JSON.stringify(collectionsToCheck));
-        db.listCollections({}, { nameOnly: true }).toArray()
-            .then((cols: any) => {
-                collectionsToCheck.forEach(collectionToCheck => {
-                    if (!cols.some((col: any) => col.name === collectionToCheck)) {
-                        console.log(`Creating ${collectionToCheck}...`);
-                        db.createCollection(collectionToCheck)
-                            .then(() => console.log(`Collection ${collectionToCheck} created.`));
-                    }
-                });
-
-                console.log('Collections validated.');
-                console.groupEnd();
-                console.groupEnd();
-
-                callback(false);
-            });
-    });
-};
 
 // Allow for processing of POSTed JSON into request.body
 app.use(express.json());
@@ -57,7 +19,7 @@ app.use(cors());
 app.use((req: any, res: any, next: any) => {
     console.group(`Handling request at ${new Date().toLocaleTimeString()} for ${req.url}...`);
     req.startedAt = new Date();
-    req.db = db;
+    req.firestore = firestore;
     next();
 });
 
@@ -74,10 +36,6 @@ app.use((req: any, res: any, next: any) => {
 });
 
 // Initialize database, and open server listener
-initializeDatabase((err) => {
-    if(!err) {
-        app.listen(API_PORT, function () {
-            console.log(`API web service started on port ${API_PORT}.`);
-        });
-    }
+app.listen(API_PORT, function () {
+    console.log(`API web service started on port ${API_PORT}.`);
 });
