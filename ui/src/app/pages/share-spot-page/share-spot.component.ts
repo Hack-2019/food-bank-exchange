9c383a1f687e4e1111bab251e0a1bafc8559308f
Joseph Naberhaus <naberhausj@gmail.com>
2019-11-03 00:08:47 -0500

    Add share product table

--- ui/src/app/pages/share-spot-page/share-spot.component.ts
+++ ui/src/app/pages/share-spot-page/share-spot.component.ts
@@ -1,10 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {environment} from "../../../environments/environment";
 import {MarketplaceEntry, MarketplaceNeed, MarketplaceProvider} from "../../../../../core/models/marketplace/marketplace"
 import {Stock, StockItem} from "../../../../../core/models/store";
 import {AuthService} from "../../auth/auth.service";
 import {MatTableDataSource} from "@angular/material/table";
+import {MatSort} from "@angular/material/sort";
+import {MatPaginator} from "@angular/material/paginator";
 
 @Component({
   selector: 'app-share-spot',
@@ -13,8 +15,22 @@ import {MatTableDataSource} from "@angular/material/table";
 })
 export class ShareSpotComponent implements OnInit {
 
-  dataSource = new MatTableDataSource();
-  displayedColumns: string[] = ['foodName', 'neededQuantity', 'ownedQuantity', 'needyUsername'];
+  canProvideDataSource = new MatTableDataSource(
+    [
+      <CanProvide>{foodName: "Loading...", neededQuantity: 0, ownedQuantity: 0, needyUsername: "Loading..."}
+    ]
+  );
+  canProvideDataSourceDisplayedColumns: string[] = ['foodName', 'neededQuantity', 'ownedQuantity', 'needyUsername'];
+
+  canReceiveDataSource= new MatTableDataSource(
+    [
+      <CanReceive>{foodName: "Loading...", offeredQuantity: 0, providerUsername: "Loading...", requiredQuantity: 0}
+    ]
+  );
+  canReceiveDataSourceDisplayedColumns: string[] = ['foodName', 'offeredQuantity', 'providerUsername', 'requiredQuantity'];
+
+  @ViewChild(MatSort, {static: true}) sort: MatSort;
+  @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
 
   httpOptions = {
     withCredentials: true
@@ -28,20 +44,24 @@ export class ShareSpotComponent implements OnInit {
 
   loadMarketplace(): void {
     this.http.get<MarketplaceEntry[]>("http://" + environment.server + "/marketplace/list", this.httpOptions).subscribe((entries: MarketplaceEntry[]) => {
+      console.log("I'm here")
       this.http.get<Stock>("http://" + environment.server + "/stock/list", this.httpOptions).subscribe((stock: Stock) => {
+        console.log("now I'm here");
         let curUser = this.auth.getAuthenticatedUser();
 
-        let canProvide: {foodName: string, neededQuantity: number, needyUsername: string, ownedQuantity}[] = [];
+        let canProvide: CanProvide[] = [];
 
         let usersNeeds: {foodName: string, quantity: number, userNeed: MarketplaceNeed}[] = [];
-        let canReceive: {foodName: string, offeredQuantity: number, providerUsername: string, userNeed: MarketplaceNeed}[] = [];
+        let canReceive: CanReceive[] = [];
 
         entries.forEach((entry) => {
           entry.needs.forEach(need => {
+            console.log(need);
             if (need.username == curUser) {
               usersNeeds.push({foodName: entry.foodName, quantity: need.quantity, userNeed: need});
             } else {
               let usersStock = stock.foods.find((food) => food.name == entry.foodName);
+              console.log(usersStock);
               if (usersStock.quantity >= 0) {
                 canProvide.push({foodName: entry.foodName, neededQuantity: need.quantity, needyUsername: need.username, ownedQuantity: usersStock.quantity});
               }
@@ -51,13 +71,32 @@ export class ShareSpotComponent implements OnInit {
 
         entries.forEach((entry) => {
           let matchingNeed = usersNeeds.find(need => need.foodName);
-          entry.providers.forEach(provider => {
-            canReceive.push({foodName: entry.foodName, offeredQuantity: provider.quantity, providerUsername: provider.username, userNeed: matchingNeed.userNeed});
-          });
+          if (matchingNeed) {
+            entry.providers.forEach(provider => {
+              if (provider.username != curUser) {
+                canReceive.push({foodName: entry.foodName, offeredQuantity: provider.quantity, providerUsername: provider.username, requiredQuantity: matchingNeed.userNeed.quantity});
+              }
+            });
+          }
         });
-
-        this.dataSource.data.push(usersNeeds);
+        console.log(canProvide);
+        this.canProvideDataSource.data = canProvide;
+        this.canReceiveDataSource.data = canReceive;
       });
     });
   }
+}
+
+interface CanProvide {
+  foodName: string;
+  neededQuantity: number;
+  needyUsername: string;
+  ownedQuantity: number;
+}
+
+interface CanReceive {
+  foodName: string;
+  offeredQuantity: number;
+  requiredQuantity: number;
+  providerUsername: string;
 }
