75a88d75896815600581181bef544f21ec822946
Sergey Gernega <pukthegreat@gmail.com>
2019-11-03 04:26:53 -0600

    added update need

--- ui/src/app/update-need-page/update-need-page.component.ts
+++ ui/src/app/update-need-page/update-need-page.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { UpdateNeed } from '../../../../core/models/marketplace/update.need'
+import { Food } from '../../../../core/models/food';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-update-need-page',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UpdateNeedPageComponent implements OnInit {
 
-  constructor() { }
+  invalidSubmissionAttempted: boolean;
+  foodNames: any;
 
-  ngOnInit() {
+  httpOptions = {
+    withCredentials: true
+  };
+  success: boolean;
+
+
+  constructor(private http: HttpClient) {
+    this.success = false;
   }
-
+  getFoods(): void {
+    this.http.get<Food[]>("http://" + environment.server + "/food/list")
+      .subscribe((foods) => {
+        this.foodNames = foods.map(food => food.name);
+      });
+  }
+  ngOnInit() {
+    this.getFoods();
+  }
+  onSubmit(names: any[], newQuantity: string) {
+    let numQuanitity = parseInt(newQuantity);
+    let request: UpdateNeed = { foodName: names[0].value, newQuantity: numQuanitity };
+    this.http.post("http://" + environment.server + "/marketplace/update/need", request, this.httpOptions).subscribe(r => {
+      this.success = true
+    });
+  }
 }
+
